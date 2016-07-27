32ce17ded0af1d33cdcd353a4cbf07e4b98c87e6
Charles Markello <cmarkell@ucsc.edu>
2016-07-27 00:17:01 +0000

    Accommodating pipes in programs.py
    Adding in docker pipes unit test
    
    Added docs to docker_call and fixed docker_call unit test
    
    Changed datasize for docker_call pipes unit test to 1GB
    
    Polished docker_call functionality
    
    Added stderr to file handle support in docker_call
    
    Factored out pipe_of_containers in docker_call

--- src/toil_scripts/lib/test/test_programs.py
+++ src/toil_scripts/lib/test/test_programs.py
@@ -1,5 +1,5 @@
 import os
-
+import re
 
 def test_docker_call(tmpdir):
     from toil_scripts.lib.programs import docker_call
@@ -12,3 +12,15 @@ def test_docker_call(tmpdir):
     with open(fpath, 'w') as f:
         docker_call(tool='ubuntu', env=dict(foo='bar'), parameters=['printenv', 'foo'], outfile=f)
     assert open(fpath).read() == 'bar\n'
+
+    # Test pipe functionality
+    # download ubuntu docker image
+    docker_call(work_dir=work_dir, tool="ubuntu")
+    command1 = ['head -c 1G /dev/urandom | tee /data/first', 'gzip', 'gunzip', 'md5sum 1>&2']
+    command2 = ['md5sum /data/first 1>&2']
+    # Test 'pipe-in-single-container' mode
+    stdout1 = docker_call(work_dir=work_dir, parameters=command1, tools='ubuntu', check_output=True, return_stderr=True)
+    stdout2 = docker_call(work_dir=work_dir, parameters=command2, tool='ubuntu', check_output=True, return_stderr=True)
+    test1 = re.findall(r"([a-fA-F\d]{32})", stdout1)
+    test2 = re.findall(r"([a-fA-F\d]{32})", stdout2)
+    assert test1[0] == test2[0]
